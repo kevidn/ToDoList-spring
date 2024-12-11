afeeebf82de4be42ba3f128a2188e2102a1c765f
RN-1007 <rayhannafishdwip@gmail.com>
2024-12-11 19:33:01 +0700

    new features

--- src/main/java/com/project/todolist/controller/TaskController.java
+++ src/main/java/com/project/todolist/controller/TaskController.java
@@ -1,12 +1,18 @@
 package com.project.todolist.controller;
 
-import com.project.todolist.service.TaskService;
-import com.project.todolist.models.Task;
-import com.project.todolist.models.Task.TaskStatus;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.todolist.models.Task;
+import com.project.todolist.models.Task.TaskStatus;
+import com.project.todolist.service.TaskService;
 
 @Controller
 @RequestMapping("/task")
@@ -55,5 +61,14 @@ public class TaskController {
     public String deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
         return "redirect:/task";
+    }
+    @PostMapping("/update-status")
+    public String updateStatus(@RequestParam Long taskId, @RequestParam String status) {
+    Task task = taskService.getTaskById(taskId)
+        .orElseThrow(() -> new RuntimeException("Task not found"));
+
+    task.setStatus(TaskStatus.valueOf(status));  // Mengupdate status task
+    taskService.save(task);  // Menyimpan task yang sudah diupdate
+    return  "redirect:/task";
     }
 }
